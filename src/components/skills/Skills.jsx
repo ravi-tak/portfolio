be7126c852dff54928b4daf8c84b89aa715b898f
RAVI KUMAR <ravikumartakajm@gmail.com>
2023-10-10 23:43:28 +0530

    Add skills

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -6,20 +6,29 @@ import { getImageUrl } from "../../utils";
 
 export const Skills = () => {
   return (
-    <section className={styles.container} id="skills">
+    <section
+      className={styles.container}
+      id="skills"
+    >
       <h2 className={styles.title}>Skills</h2>
       <div className={styles.content}>
         <div className={styles.skills}>
-          {skills.map((skill, id) =>
-              <div key={id} className={styles.skill}>
-                <div className={styles.skillImageContainer}>
-                  <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
-                </div>
-                <p>{skill.title}</p>
+          {skills.map((skill, id) => (
+            <div
+              key={id}
+              className={styles.skill}
+            >
+              <div className={styles.skillImageContainer}>
+                <img
+                  src={getImageUrl(skill.imageSrc)}
+                  alt={skill.title}
+                />
               </div>
-          )}
+              <p>{skill.title}</p>
+            </div>
+          ))}
         </div>
       </div>
     </section>
-  )
+  );
 };
